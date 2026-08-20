71be67070029d48bdacbc41b7e418483c326b777
GodjoseSLMLK <josejos1102x@gmil.com>
2026-08-19 19:36:16 -0500

    subiendo app de mi clase

---
+++ src/MiClase.java
@@ -0,0 +1,12 @@
+public class MiClase{
+    private int edad = 16;
+    private double altura = 1.78;
+    private char sexo = 'M';
+    private boolean aprobado = true;
+    String nombre = "JoseVega";
+    String saludo = "Hola" + nombre;
+    double precio = 9.99;
+    int precioCortado = (int) precio;
+    int entero = 5;
+    double decimal = entero;
+}
